aa5fbfd64407c5e8e599b4c5642cd916b05927f2
honmakousuke <kou1234ske@hotmail.com>
2018-09-08 14:36:19 +0900

    basicninshou

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,19 @@
 import $ from 'jquery';
+window.$ = $;
+window.jQuery = $;
 import 'slick-carousel';
 import '../../node_modules/slick-carousel/slick/slick.css';
 import '../../node_modules/slick-carousel/slick/slick-theme.css';
 
-$('button').on('click', () => {
-    window.alert('hello webpack');
+$(function(){
+    // #で始まるリンクをクリックしたら実行されます
+    $('header a[href^="#"]').click(function() {
+      // スクロールの速度
+      var speed = 500; // ミリ秒で記述
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+      return false;
+    });
 });
--- src/js/test.js
+++
@@ -1,8 +0,0 @@
-var tickets = [ "指定席", "自由席", "指定席", "自由席", "指定席" ];
-for (var i = 0 ; i < tickets.length; i++) {
-    if(tickets[i] == "自由席" ) {
-        continue;
-    }
-    console.log(i + " : " + tickets[i]);
-}
-console.log('end');
